cb0446e699ed7757cd943f90ecfd8d9b062bf620
AVR-GCC <ogoun.d@gmail.com>
2025-01-02 00:44:50 +0200

    LSP config and rust analyzer

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -16,6 +16,30 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "rust_analyzer" },
+})
+local nvim_lsp = require('lspconfig')
+local on_attach = function(client, bufnr)
+  -- Here you can define your key mappings or other behavior when LSP attaches
+end
+
+nvim_lsp.rust_analyzer.setup({
+  on_attach = on_attach,
+  settings = {
+    ['rust-analyzer'] = {
+      checkOnSave = {
+        command = "clippy"
+      },
+    }
+  }
+})
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+nvim_lsp.rust_analyzer.setup({
+  capabilities = capabilities,
+  -- ... other settings from above ...
+})
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,18 +8,15 @@ return {
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       require "configs.lspconfig"
     end,
   },
+  { 'williamboman/mason.nvim', event = { "BufReadPre", "BufNewFile" } },
+  { 'williamboman/mason-lspconfig.nvim', event = { "BufReadPre", "BufNewFile" } },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  -- Completion (optional but recommended)
+  { 'hrsh7th/nvim-cmp', event = "InsertEnter" },
+  { 'hrsh7th/cmp-nvim-lsp', event = "InsertEnter", after = "nvim-cmp" },
 }
