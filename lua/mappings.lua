904cf56c98ea78c94ea33f90fc002365dc8cb756
AVR-GCC <ogoun.d@gmail.com>
2025-01-02 00:46:13 +0200

    Easy escape terminal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,5 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("t", "<C-w>", "<C-\\><C-n>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
