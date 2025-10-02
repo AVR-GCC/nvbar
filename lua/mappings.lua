63fc228a3a4333484dee8a6b949dd0426b5ffc43
AVR-GCC <ogoun.d@gmail.com>
2025-10-02 04:04:58 +0300

    Divide too long lines

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,5 +9,7 @@ map("i", "jk", "<ESC>")
 
 map("t", "<C-w>", "<C-\\><C-n>")
 map("n", "<leader>u", "viw\"_dhp")
+map("n", "<leader>j", "f s<enter><ESC>")
+map("n", "<leader>k", "f,a<enter><ESC>")
 map('n', '<leader>lg', '<cmd>LazyGit<CR>', { noremap = true, silent = true })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
